79759960865539c4ae33f94836c69e521cd480e2
jayesh-backend <jayeshgupta+88@garageplug.com>
2025-03-04 09:25:27 +0530

    added main class code

--- src/WithStrategyPattern/Main.java
+++ src/WithStrategyPattern/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        Vehicle normalVehicle = new PassengerVehicle();
+//        normalVehicle.drive();
+
+//        Vehicle offRoadVehicle = new OffRoadVehicle();
+//        offRoadVehicle.drive();
+
+//        Vehicle sportVehicle = new SportVehicle();
+//        sportVehicle.drive();
     }
 }
